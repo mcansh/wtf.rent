21e278c19d4d99cf6b615db6ed2d1cdc33a96115
Logan McAnsh <logan@mcan.sh>
2021-10-25 14:44:49 -0400

    chore: add mobile viewport

--- app/root.tsx
+++ app/root.tsx
@@ -20,6 +20,7 @@ function Document({
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.png" type="image/png" />
         {title ? <title>{title}</title> : null}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
