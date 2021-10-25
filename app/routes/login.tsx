a048ab4aded2d2fe8c2a1f2bdcee53b904b9c025
Logan McAnsh <logan@mcan.sh>
2021-10-25 17:24:27 -0400

    fix(login): use proper name

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -100,8 +100,8 @@ const LoginPage: RouteComponent = () => {
                 <div className="mt-1">
                   <input
                     id="username"
-                    name="text"
-                    type="username"
+                    name="username"
+                    type="text"
                     autoComplete="username"
                     required
                     className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
