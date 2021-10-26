3514249aaba6f65e496c97eff3f991896754ab46
Logan McAnsh <logan@mcan.sh>
2021-10-25 20:56:28 -0400

    chore: add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
