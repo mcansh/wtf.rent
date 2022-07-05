b31c747da554113ad3f0d74ebe0c755cfeb53350
Logan McAnsh <logan@mcan.sh>
2022-07-05 19:00:07 -0400

    chore: use session secret
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/session.server.ts
+++ app/session.server.ts
@@ -1,12 +1,16 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 
+if (!process.env.SESSION_SECRET) {
+  throw new Error("SESSION_SECRET is not defined");
+}
+
 export let sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "_session",
     sameSite: "lax",
     path: "/",
     httpOnly: true,
-    secrets: ["s3cr3t"],
+    secrets: [process.env.SESSION_SECRET],
     secure: process.env.NODE_ENV === "production",
   },
 });
