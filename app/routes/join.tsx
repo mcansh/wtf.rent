c3d3d3d825629aa43e7c4248bdee6741272c5971
Logan McAnsh <logan@mcan.sh>
2022-07-06 08:54:16 -0400

    chore: add link to join from login
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -4,7 +4,7 @@ import type {
   MetaFunction,
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData, useTransition } from "@remix-run/react";
+import { Form, Link, useActionData, useTransition } from "@remix-run/react";
 import { useForm, useFieldset, conform } from "@conform-to/react";
 import { resolve, parse } from "@conform-to/zod";
 import z from "zod";
@@ -148,162 +148,161 @@ export default function JoinPage() {
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-          <Form method="post" {...formProps}>
-            <fieldset
-              className="space-y-6"
-              disabled={!!pendingForm}
-              {...fieldsetProps}
-            >
-              <div>
+        <Form
+          method="post"
+          {...formProps}
+          className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10"
+        >
+          <fieldset
+            className="space-y-6"
+            disabled={!!pendingForm}
+            {...fieldsetProps}
+          >
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  autoComplete="email"
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  aria-errormessage={
+                    result.email.error ? "email-error" : undefined
+                  }
+                  {...conform.input(result.email, { type: "email" })}
+                />
+              </div>
+              {result.email.error && (
+                <div id="email-error" className="mt-2 text-sm text-red-600">
+                  {result.email.error}
+                </div>
+              )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <div className="mt-1">
+                <input
+                  id="username"
+                  autoComplete="username"
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  aria-errormessage={
+                    result.username.error ? "username-error" : undefined
+                  }
+                  {...conform.input(result.username, { type: "text" })}
+                />
+              </div>
+              {result.username.error && (
+                <div id="username-error" className="mt-2 text-sm text-red-600">
+                  {result.username.error}
+                </div>
+              )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  autoComplete="new-password"
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  aria-errormessage={
+                    result.password.error ? "password-error" : undefined
+                  }
+                  {...conform.input(result.password, { type: "password" })}
+                />
+              </div>
+              {result.password.error && (
+                <div id="password-error" className="mt-2 text-sm text-red-600">
+                  {result.password.error}
+                </div>
+              )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="passwordConfirm"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password confirmation
+              </label>
+              <div className="mt-1">
+                <input
+                  id="passwordConfirm"
+                  autoComplete="new-password"
+                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  aria-errormessage={
+                    result.passwordConfirm.error
+                      ? "passwordConfirm-error"
+                      : undefined
+                  }
+                  {...conform.input(result.passwordConfirm, {
+                    type: "password",
+                  })}
+                />
+              </div>
+              {result.passwordConfirm.error && (
+                <div
+                  id="passwordConfirm-error"
+                  className="mt-2 text-sm text-red-600"
+                >
+                  {result.passwordConfirm.error}
+                </div>
+              )}
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
                 <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
                 >
-                  Email address
+                  Remember me
                 </label>
-                <div className="mt-1">
-                  <input
-                    id="email"
-                    autoComplete="email"
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    aria-errormessage={
-                      result.email.error ? "email-error" : undefined
-                    }
-                    {...conform.input(result.email, { type: "email" })}
-                  />
-                </div>
-                {result.email.error && (
-                  <div id="email-error" className="mt-2 text-sm text-red-600">
-                    {result.email.error}
-                  </div>
-                )}
               </div>
 
-              <div>
-                <label
-                  htmlFor="username"
-                  className="block text-sm font-medium text-gray-700"
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
                 >
-                  Username
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="username"
-                    autoComplete="username"
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    aria-errormessage={
-                      result.username.error ? "username-error" : undefined
-                    }
-                    {...conform.input(result.username, { type: "text" })}
-                  />
-                </div>
-                {result.username.error && (
-                  <div
-                    id="username-error"
-                    className="mt-2 text-sm text-red-600"
-                  >
-                    {result.username.error}
-                  </div>
-                )}
+                  Forgot your password?
+                </a>
               </div>
+            </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    autoComplete="new-password"
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    aria-errormessage={
-                      result.password.error ? "password-error" : undefined
-                    }
-                    {...conform.input(result.password, { type: "password" })}
-                  />
-                </div>
-                {result.password.error && (
-                  <div
-                    id="password-error"
-                    className="mt-2 text-sm text-red-600"
-                  >
-                    {result.password.error}
-                  </div>
-                )}
-              </div>
-
-              <div>
-                <label
-                  htmlFor="passwordConfirm"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password confirmation
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="passwordConfirm"
-                    autoComplete="new-password"
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    aria-errormessage={
-                      result.passwordConfirm.error
-                        ? "passwordConfirm-error"
-                        : undefined
-                    }
-                    {...conform.input(result.passwordConfirm, {
-                      type: "password",
-                    })}
-                  />
-                </div>
-                {result.passwordConfirm.error && (
-                  <div
-                    id="passwordConfirm-error"
-                    className="mt-2 text-sm text-red-600"
-                  >
-                    {result.passwordConfirm.error}
-                  </div>
-                )}
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Join
-                </button>
-              </div>
-            </fieldset>
-          </Form>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Join
+              </button>
+            </div>
+          </fieldset>
+        </Form>
+        <div className="mt-4 text-center font-medium text-indigo-600 hover:text-indigo-500">
+          <Link to="/login">Already have an account? Login here.</Link>
         </div>
       </div>
     </div>
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -6,6 +6,7 @@ import type {
 import { redirect } from "@remix-run/node";
 import {
   Form,
+  Link,
   useActionData,
   useLocation,
   useTransition,
@@ -108,6 +109,10 @@ export let meta: MetaFunction = () => ({
   title: "Log in",
 });
 
+export let handle: RouteHandle = {
+  bodyClassName: "bg-gray-50",
+};
+
 export default function LoginPage() {
   let location = useLocation();
   let transition = useTransition();
@@ -129,116 +134,115 @@ export default function LoginPage() {
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-          <Form
-            method="post"
-            action={location.pathname + location.search}
-            {...formProps}
+        <Form
+          method="post"
+          action={location.pathname + location.search}
+          {...formProps}
+          className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10"
+        >
+          <fieldset
+            className="space-y-6"
+            disabled={!!pendingForm}
+            {...fieldsetProps}
           >
-            <fieldset
-              className="space-y-6"
-              disabled={!!pendingForm}
-              {...fieldsetProps}
-            >
-              <div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  autoComplete="email"
+                  className={clsx(
+                    "block w-full appearance-none rounded-md border px-3 py-2 shadow-sm focus:outline-none sm:text-sm",
+                    result.email.error
+                      ? "border-red-300 placeholder-red-400 focus:border-red-500 focus:ring-red-500"
+                      : "border-gray-300 placeholder-gray-400 focus:border-indigo-500 focus:ring-indigo-500"
+                  )}
+                  aria-errormessage={
+                    result.email.error ? "email-error" : undefined
+                  }
+                  {...conform.input(result.email, { type: "email" })}
+                />
+              </div>
+              {result.email.error && (
+                <div id="email-error" className="mt-2 text-sm text-red-600">
+                  {result.email.error}
+                </div>
+              )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  autoComplete="new-password"
+                  className={clsx(
+                    "block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none  sm:text-sm",
+                    result.password.error
+                      ? "border-red-300 placeholder-red-400 focus:border-red-500 focus:ring-red-500"
+                      : "border-gray-300 placeholder-gray-400 focus:border-indigo-500 focus:ring-indigo-500"
+                  )}
+                  aria-errormessage={
+                    result.password.error ? "password-error" : undefined
+                  }
+                  {...conform.input(result.password, { type: "password" })}
+                />
+              </div>
+              {result.password.error && (
+                <div id="password-error" className="mt-2 text-sm text-red-600">
+                  {result.password.error}
+                </div>
+              )}
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
                 <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
                 >
-                  Email
+                  Remember me
                 </label>
-                <div className="mt-1">
-                  <input
-                    id="email"
-                    autoComplete="email"
-                    className={clsx(
-                      "block w-full appearance-none rounded-md border px-3 py-2 shadow-sm focus:outline-none sm:text-sm",
-                      result.email.error
-                        ? "border-red-300 placeholder-red-400 focus:border-red-500 focus:ring-red-500"
-                        : "border-gray-300 placeholder-gray-400 focus:border-indigo-500 focus:ring-indigo-500"
-                    )}
-                    aria-errormessage={
-                      result.email.error ? "email-error" : undefined
-                    }
-                    {...conform.input(result.email, { type: "email" })}
-                  />
-                </div>
-                {result.email.error && (
-                  <div id="email-error" className="mt-2 text-sm text-red-600">
-                    {result.email.error}
-                  </div>
-                )}
               </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
                 >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    autoComplete="new-password"
-                    className={clsx(
-                      "block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none  sm:text-sm",
-                      result.password.error
-                        ? "border-red-300 placeholder-red-400 focus:border-red-500 focus:ring-red-500"
-                        : "border-gray-300 placeholder-gray-400 focus:border-indigo-500 focus:ring-indigo-500"
-                    )}
-                    aria-errormessage={
-                      result.password.error ? "password-error" : undefined
-                    }
-                    {...conform.input(result.password, { type: "password" })}
-                  />
-                </div>
-                {result.password.error && (
-                  <div
-                    id="password-error"
-                    className="mt-2 text-sm text-red-600"
-                  >
-                    {result.password.error}
-                  </div>
-                )}
+                  Forgot your password?
+                </a>
               </div>
+            </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Sign in
-                </button>
-              </div>
-            </fieldset>
-          </Form>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Sign in
+              </button>
+            </div>
+          </fieldset>
+        </Form>
+        <div className="mt-4 text-center font-medium text-indigo-600 hover:text-indigo-500">
+          <Link to="/join">New Here? Join now.</Link>
         </div>
       </div>
     </div>
