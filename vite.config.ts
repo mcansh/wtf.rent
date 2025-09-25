fa8ab44c392bff03aac8344d300c249df9da6c5a
Logan McAnsh <logan@mcan.sh>
2025-09-25 22:23:14 +0000

    feat: add denyImports vite plugin
    https://reactrouter.com/how-to/react-server-components#serverclient-modules
    Signed-off-by: GitHub <noreply@github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import rsc from "@vitejs/plugin-rsc";
 import { svgSprite } from "@mcansh/vite-plugin-svg-sprite";
 import { unstable_reactRouterRSC as reactRouterRSC } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import rsc from "@vitejs/plugin-rsc";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { denyImports } from "vite-env-only";
 import devtoolsJson from "vite-plugin-devtools-json";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(),
+  plugins: [
+    tailwindcss(),
     tsconfigPaths(),
     svgSprite(),
     reactRouterRSC(),
     rsc(),
     devtoolsJson(),
-  ]
+    denyImports({
+      client: { files: ["**/.server/*", "**/*.server.*"] },
+    }),
+  ],
 });
