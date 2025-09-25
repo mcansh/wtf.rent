dd1a0b48fe5cb80cb7fb88acd44658aa1271f5b1
Logan McAnsh <logan@mcan.sh>
2025-09-25 22:11:23 +0000

    feat: implement user authentication check on logout
    Signed-off-by: GitHub <noreply@github.com>

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,12 +1,28 @@
-import { redirect } from "react-router";
+import { Form } from "react-router";
 
-import { logout } from "~/session.server";
+import { logout, requireUserId } from "~/session.server";
 import type { Route } from "./+types/logout";
 
 export async function action({ request }: Route.ActionArgs) {
   return logout(request);
 }
 
-export function loader() {
-  return redirect("/");
+export async function loader({request}: Route.LoaderArgs) {
+  await requireUserId(request, "/");
+  return null
+}
+
+export default function LogoutPage() {
+  return (
+    <div className="mx-auto h-full grid place-items-center max-w-7xl px-2 sm:px-6 lg:px-8">
+      <Form method="post">
+        <button
+          type="submit"
+          className="rounded bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Log out
+        </button>
+      </Form>
+    </div>
+  );
 }
