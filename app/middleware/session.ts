474703763d0ee347a6f27f3e94c39d08e75b4f77
Logan McAnsh <logan@mcan.sh>
2026-09-01 17:49:23 -0400

    fixup! feat: use redis for sessions

--- app/middleware/session.ts
+++ app/middleware/session.ts
@@ -1,9 +1,9 @@
-import { createClient } from "redis"
 import { createCookie } from "remix/cookie"
 import { Session } from "remix/session"
 import { createRedisSessionStorage } from "remix/session-storage/redis"
 
 import { env } from "../env.ts"
+import { redis } from "../redis.ts"
 
 export const sessionCookie = createCookie("session", {
   secrets: env.SESSION_SECRETS,
@@ -14,11 +14,6 @@ export const sessionCookie = createCookie("session", {
   secure: env.NODE_ENV === "production",
 })
 
-const redis = createClient({
-  url: env.REDIS_URL,
-  keyPrefix: "session:",
-})
-
-export const sessionStorage = createRedisSessionStorage(redis)
+export const sessionStorage = createRedisSessionStorage(redis, { keyPrefix: "session:" })
 
 export { Session }
